b89f4a3ec4c1d41354d372ee6e6b89899dcbcada
= <danielhoman31@gmail.com>
2018-11-29 11:45:03 +0000

    Added the decrypt function to the program.

--- extra_practice_ciphers/vigenere.py
+++ extra_practice_ciphers/vigenere.py
@@ -30,8 +30,34 @@ def encrypt():
 
     return c
 
-def decrypt(c):
-    p = None
+def decrypt():
+    p = ""
+    print("Please enter your ciphertext: ")
+    ciphertext = input()
+    ciphertext = ciphertext.lower()
+    print("Enter your key: ")
+    key = input()
+
+    ciphertext = ciphertext.replace(' ','')
+    repeating_key = ""
+    while len(repeating_key)<len(ciphertext):
+        repeating_key += key
+
+    # shrink the key to the size of the plaintext
+    repeating_key = repeating_key[:len(ciphertext)]
+
+    # make character comparisons
+    for i in range(len(ciphertext)):
+        x = alphabet.index(ciphertext[i])            # returns the ascii value of i in c
+        print(x)
+        y = alphabet.index(repeating_key[i])        # returns the ascii value of i in k
+        print(y)
+        if x<y:
+            x+=26
+        z = abs(x-y)
+        print(z)
+        p+=alphabet[z]
+
     return p
 
 if __name__ == '__main__':
@@ -39,13 +65,15 @@ if __name__ == '__main__':
     while True:
         try:
             ans = int(input())
+            print("Answer:", type(ans))
             if ans == 1:
                 c = encrypt()
                 print(c)
                 break
             elif ans == 2:
+                print("about to run decrypt")
                 p = decrypt()
-                print(type(p))
+                print(p)
                 break
             else:
                 print("Please enter 1 or 2.")
